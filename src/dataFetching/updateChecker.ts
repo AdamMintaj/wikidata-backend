480739c646fb271d7619bcf3c6f6e1fa25a02db7
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-06-10 17:31:16 +0200

    add update checker functions

---
+++ src/dataFetching/updateChecker.ts
@@ -0,0 +1,48 @@
+import { queryDB } from "../db.js";
+import { Timestamp } from "./types.js";
+
+/**
+ * Sends a query to the database to get the date of the last data fetch from WikiData.
+ *
+ * In a rare scenario where the database has no fetch logs it returns undefined.
+ * @returns Promise that resolves to a Date or undefined
+ */
+async function getLastWikiDataFetchDate(): Promise<Date | undefined> {
+  const query =
+    "SELECT timestamp FROM wikidata_fetch_logs ORDER BY id DESC LIMIT 1;";
+  const [response] = await queryDB<Timestamp>(query);
+  return response.timestamp;
+}
+
+/**
+ * Checks if the database should be updated and returns an appropriate boolean value.
+ * Internally it calls getLastWikiDataFetchDate and calculates the time that passed since.
+ * If the time exceeds the update period or there was no previous data fetch it returns true.
+ *
+ * The update period is expected to come from an environment variable, otherwise it defaults to 7 days.
+ * @param DATABASE_UPDATE_PERIOD env variable
+ * @returns Promise that resolves to a boolean
+ */
+async function checkIfUpdateNeeded(): Promise<boolean> {
+  let lastWikiDataFetchDate: Date | null;
+
+  try {
+    const date = await getLastWikiDataFetchDate();
+    lastWikiDataFetchDate = date ?? null;
+  } catch (error) {
+    console.warn("Getting last wikiData fetch date failed", error);
+    return false;
+  }
+
+  if (!lastWikiDataFetchDate) return true;
+
+  const previousFetchDateMs = lastWikiDataFetchDate.getTime();
+
+  const DEFAULT_UPDATE_PERIOD = 7 * 24 * 60 * 60 * 1000; // days * hours * minutes * seconds * miliseconds
+  const updatePeriod =
+    Number(process.env.DATABASE_UPDATE_PERIOD) || DEFAULT_UPDATE_PERIOD;
+
+  return Date.now() - previousFetchDateMs > updatePeriod;
+}
+
+export default checkIfUpdateNeeded;
