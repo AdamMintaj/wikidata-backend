3747e1198a1f2eefcee072b67b640289d76cf68f
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-06-11 17:03:23 +0200

    rename directory

--- src/dbUpdater/processingData.ts
+++ src/dbUpdater/processingData.ts
@@ -6,35 +6,6 @@ import {
 import { Entity, UnfilteredWikidata } from "./types.js";
 
 /**
- * Data returned from Wikidata query may have multiple entries for the same entity -
- * and usually they only differ in height value.
- *
- * This function takes formatted data returned by `extractData` function and gets rid
- * of duplicate entries, leaving only the entry with the highest height value.
- * @param data Array of `Entity` objects
- * @returns Array of `Entity` objects, without duplicate entries
- */
-export function deduplicateDataByHeight(data: Entity[]) {
-  const entitiesMap = new Map<string, Entity>();
-
-  for (const entity of data) {
-    const existingEntity = entitiesMap.get(entity.id);
-
-    if (!existingEntity) {
-      entitiesMap.set(entity.id, entity);
-    } else {
-      const previousHeight = existingEntity.height;
-      const newHeight = entity.height;
-      entitiesMap.set(entity.id, {
-        ...entity,
-        height: Math.max(previousHeight, newHeight),
-      });
-    }
-  }
-  return [...entitiesMap.values()];
-}
-
-/**
  * Extracts and formats relevant information from raw Wikidata response.
  * Each returned object represents a single, complete database entry.
  * Entries with missing or invalid data are excluded from the returned array.
@@ -61,4 +32,33 @@ export function extractData(data: UnfilteredWikidata) {
   });
 
   return extractedData.filter((item) => item != undefined);
+}
+
+/**
+ * Data returned from Wikidata query may have multiple entries for the same entity -
+ * and usually they only differ in height value.
+ *
+ * This function takes formatted data returned by `extractData` function and gets rid
+ * of duplicate entries, leaving only the entry with the highest height value.
+ * @param data Array of `Entity` objects
+ * @returns Array of `Entity` objects, without duplicate entries
+ */
+export function deduplicateDataByHeight(data: Entity[]) {
+  const entitiesMap = new Map<string, Entity>();
+
+  for (const entity of data) {
+    const existingEntity = entitiesMap.get(entity.id);
+
+    if (!existingEntity) {
+      entitiesMap.set(entity.id, entity);
+    } else {
+      const previousHeight = existingEntity.height;
+      const newHeight = entity.height;
+      entitiesMap.set(entity.id, {
+        ...entity,
+        height: Math.max(previousHeight, newHeight),
+      });
+    }
+  }
+  return [...entitiesMap.values()];
 }
