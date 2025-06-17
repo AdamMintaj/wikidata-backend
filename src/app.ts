c490952d4e07ec25d2980d94ca1573df3566e76f
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-06-17 17:07:53 +0200

    Call updateDatabase from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express from "express";
 
+import updateDatabase from "./dbUpdater/controller.js";
+
 const app = express();
+
+updateDatabase().catch((error: unknown) => {
+  console.error(error);
+});
+
+app.get("/", (req, res) => {
+  res.send("elo");
+});
 
 export default app;
