00e6ea0011faf8b1d3c344cec7e08c84b811d154
Adam Maliszewski <maliszewski.aa@gmail.com>
2025-06-23 11:28:17 +0200

    Add server's listening port as env variable

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("started server at port 3000");
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log(`started server at port ${PORT.toString()}`);
 });
